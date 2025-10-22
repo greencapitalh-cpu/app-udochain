fc479951ed45d82bf2e21b11182b59c5449108e9
greencapitalh-cpu <greencapitalh@gmail.com>
2025-10-22 15:57:54 -0600

    Create Invitations.tsx

---
+++ src/pages/Invitations.tsx
@@ -0,0 +1,7 @@
+// [31] src/pages/Invitations.tsx
+import EmptyState from "../ui/EmptyState";
+
+export default function Invitations() {
+  // TODO: fetch listado de invitaciones
+  return <EmptyState title="No invitations yet" text="Create and send invitations from Sign or Vote." />;
+}
